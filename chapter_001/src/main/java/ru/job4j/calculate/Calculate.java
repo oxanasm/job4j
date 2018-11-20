f84a7b2a165c4935cc9410dfc27265ccd3cbe255
Oksana Smelyanskaya <smelyanskaya@mail.ru>
2018-11-20 22:06:37 +0700

    1.5. Checkstyle[#81269]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,7 +9,7 @@ public class Calculate {
      * Main.
      * @param args - args.
      */
-	public static void main (String [] args) {
+	public static void main(String[]args) {
 		System.out.println("Hello World");
 	}
 	
