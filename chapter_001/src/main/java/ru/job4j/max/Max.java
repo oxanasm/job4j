26e2a342ad900d9b93daadd4e6fe093b3cdbb15d
Oksana Smelyanskaya <smelyanskaya@mail.ru>
2018-11-29 23:23:48 +0700

    4.2. Максимум из двух чисел[#81230]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,11 @@
+package ru.job4j.max;
+
+public class Max {
+    /**
+     * Отвечает на вопросы.
+     * @return Ответ.
+     */
+    public int max(int first, int second){
+       return (first > second) ? first : second;
+    }
+}
