7d530172d5c8ee5e9fd4f0ab04a3d0de5034ee5e
Oksana Smelyanskaya <smelyanskaya@mail.ru>
2018-12-15 21:11:33 +0700

    5.5. Рефакторинг кода.[#81235]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,15 +1,23 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
 /**
- * шахматная доска
+ * пирамида
  */
 public class Paint {
     public String pyramid(int h) {
+        return this.loopBy(
+                h,
+                2 * h - 1,
+                (row, column) -> row >= h - column - 1 && row + h - 1 >= column
+        );
+    }
+
+    private String loopBy(int h, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int w = 2 * h - 1;
         for (int row = 0; row != h; row++) {
-            for (int column = 0; column != w; column++) {
-                if (row >= h - column - 1 && row + h - 1 >= column) {
+            for (int column = 0; column != weight; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -18,5 +26,6 @@ public class Paint {
             screen.append(System.lineSeparator());
         }
         return screen.toString();
+
     }
 }
